1f56797602506ca99f802aa11cc6fa2bd078625c
haoxl3 <1098262516@qq.com>
2018-04-03 22:25:33 +0800

    koa-view

--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
 const Koa = require('koa')
 const app = new Koa()
-const {normal} = require('./tpl')
+// const {htmlTp, ejsTpl, pugTpl} = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
 
-app.use(async(ctx, next) => {
-    ctx.type = 'text/html;charset=utf-8'
-    ctx.body = normal
+// ejs
+// app.use(async(ctx, next) => {
+//     ctx.type = 'text/html;charset=utf-8'
+//     ctx.body = ejs.render(ejsTpl, {
+//         you: 'world',
+//         me: 'haoxl'
+//     })
+// })
+
+// pug
+// app.use(async(ctx, next) => {
+//     ctx.type = 'text/html;charset=utf-8'
+//     ctx.body = pug.render(pugTpl, {
+//         you: 'world',
+//         me: 'haoxl'
+//     })
+// })
+//koa-views包括了几种模板引擎，此项目用pug
+const views = require('koa-views')
+const {resolve} = require('path')
+
+//去views找所有扩展名为pug的
+app.use(views(resolve(__dirname, './views'),{
+    extension: 'pug'
+}))
+app.use(async(ctx, next)=>{
+    await ctx.render('index', {
+        you: 'world',
+        me: 'haoxl'
+    })
 })
 app.listen(8899)
